424d38aa6962fc92fed91c654c7e0dd701f532bf
suhail-ibne-habib <suhailibnehabib@gmail.com>
2025-01-09 20:55:09 +0600

    fixed slick slider

--- js/main.js
+++ js/main.js
@@ -34,11 +34,12 @@ if (parallaxYs) {
         gsap.set(parallaxY, { y: -100 });
 
         gsap.to( parallaxY, {
-            ease: 'none',
+            ease: "slow",
             y: 100,
             scrollTrigger: {
                 trigger: parallaxY,
-                start: "top 80%",
+                start: "top center",
+                end: "end top",
                 toggleActions: 'play none none none',
                 scrub: true
             }
@@ -54,7 +55,7 @@ if (parallaxXs) {
         gsap.set(parallaxX, {y: 100})
 
         gsap.to( parallaxX, {
-            ease: 'none',
+            ease: 'slow',
             y: -100,
             scrollTrigger: {
                 trigger: parallaxX,
@@ -67,27 +68,51 @@ if (parallaxXs) {
     });
 }
 
-const slickScrollers = document.querySelectorAll('.slick-scroller')
+// const slickScrollers = document.querySelectorAll('.slick-scroller')
 
-if( slickScrollers ){
-    slickScrollers.forEach( scroller => {
+// if( slickScrollers ){
+//     slickScrollers.forEach( scroller => {
         
-        $(document).ready(function() {
-            // Initialize all slick-scrollers
-            $('.slick-scroller').each(function() {
-                const scroller = $(this);
+//         $(document).ready(function() {
+//             // Initialize all slick-scrollers
+//             $('.slick-scroller').each(function() {
+//                 const scroller = $(this);
                 
-                // Initialize the main slider for each scroller
-                scroller.slick({
-                    slidesToShow: 1,
-                    slidesToScroll: 1,
-                    arrows: false, // Disable default navigation arrows
-                });
+//                 // Initialize the main slider for each scroller
+//                 scroller.slick({
+//                     slidesToShow: 1,
+//                     slidesToScroll: 1,
+//                     arrows: false, // Disable default navigation arrows
+//                 });
         
-            });
-        });
+//             });
+//         });
         
         
 
+//     })
+// }
+
+const slickSliders = document.querySelectorAll('.s-slider')
+
+if( slickSliders ){
+
+    slickSliders.forEach( slider => {
+        const slidesToShow = slider.dataset.slidesShow;
+        const slidesArrows = `.${slider.getAttribute('data-arrows') || 'arrows'}`
+        const slidesPaging = `.${slider.getAttribute('data-paging') || 'paging'}`
+        console.log(slidesPaging)
+
+        $(slider).slick({
+            infinite: false,
+            slidesToShow: +slidesToShow,
+            slidesToScroll: 1,
+            dots: true,
+            appendArrows:  $(slidesArrows),
+            appendDots: $(slidesPaging),
+            prevArrow: '<button title="View previous item" class="s-slick-btn View previous item" aria-label="View previous item" data-action="Prev"><svg viewBox="0 0 24 24" class="blk icn " role="presentation" data-use="/cms/svg/site/h4t30ckkkjq.24.2501071125102.svg#arrow_left"><path d="M18.181 23.798a0.686 0.686 0 0 1-0.48 0.202a0.666 0.666 0 0 1-0.48-0.202L5.803 12.488a0.668 0.668 0 0 1 0-0.96L17.214 0.215a0.69 0.69 0 0 1 0.985-0.016A0.674 0.674-13.217 0 1 18.181 1.175L7.255 12.008L18.181 22.839a0.672 0.672 0 0 1 0 0.96Z"></path></svg></button>',
+            nextArrow: '<button title="View next item" aria-label="View next item" data-action="Next"><svg viewBox="0 0 24 24" class="blk icn " role="presentation" data-use="/cms/svg/site/h4t30ckkkjq.24.2501071125102.svg#arrow_right"><path d="M5.821 23.798a0.686 0.686 0 0 0 0.48 0.202a0.666 0.666 0 0 0 0.48-0.202L18.195 12.488a0.668 0.668 0 0 0 0-0.96L6.785 0.215a0.69 0.69 0 0 0-0.985-0.016A0.674 0.674-13.218 0 0 5.821 1.175L16.744 12.008L5.821 22.839a0.672 0.672 0 0 0 0 0.96Z"></path></svg></button>'
+        })
     })
+
 }
